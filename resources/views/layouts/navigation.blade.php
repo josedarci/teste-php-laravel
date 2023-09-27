9cf719619568d08cda1587318e6d095d2f8cec7a
José Darci Rodrigues Junior <juniorinternet@gmail.com>
2023-09-27 13:59:27 -0300

    :sparkles: feat: Inserido option no select do menu para ir para rota import/index

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,7 +37,9 @@
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
-
+                        <x-dropdown-link :href="route('import.index')">
+                            {{ __('Importar Json') }}
+                        </x-dropdown-link>
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
